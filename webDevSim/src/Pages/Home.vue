ef9c27296e847f2de558b9c5e0df4cdf89fca3b9
shayd3 <ryan.ross215@gmail.com>
2024-06-05 22:11:38 -0400

    Fix toggle

--- webDevSim/src/Pages/Home.vue
+++ webDevSim/src/Pages/Home.vue
@@ -6,15 +6,18 @@ import AudioPlusResumeUpload from '../components/audio.vue'
 import { ref } from 'vue';
 import Popup from '../components/submissionPopUp.vue';
 export default {
-    setup () {
-        let popupTriggers = ref({
-            buttonTrigger: true
-        });
+    data () {
         return {
-            Popup,
-            popupTriggers,
-         }
-    }
+            popupTriggers: {
+                buttonTrigger: false
+            }
+        }
+    },
+    methods: {
+    togglePopup() {
+      this.popupTriggers.buttonTrigger = !this.popupTriggers.buttonTrigger;
+    },
+  },
 }
 
 </script>
@@ -22,10 +25,10 @@ export default {
 <template>
     <main>
         <AudioPlusResumeUpload/>
-        <button>
+        <button @click="togglePopup">
             <h4>Open Popup</h4>
         </button>
-        <Popup v-if="popupTriggers.buttonTrigger">
+        <Popup v-if="popupTriggers && popupTriggers.buttonTrigger">
             <h2>idk</h2>
         </Popup>
     </main>
